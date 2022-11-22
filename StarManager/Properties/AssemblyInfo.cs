711e97e3ea484871298dffd9317239cde16e8ee3
aglab2 <aglab3@gmail.com>
2022-11-23 07:34:28 +0800

    Upgrade octokit to latest version, dotnet==4.6.2

--- StarManager/Properties/AssemblyInfo.cs
+++ StarManager/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.24.0")]
-[assembly: AssemblyFileVersion("1.24.0")]
+[assembly: AssemblyVersion("1.25.0")]
+[assembly: AssemblyFileVersion("1.25.0")]
